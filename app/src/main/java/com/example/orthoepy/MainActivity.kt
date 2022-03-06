5c657405ab9f894f0b3c38f07d9d21b1a877f0a4
sl1mslav <dm.partilov@gmail.com>
2022-03-07 00:13:38 +0500

    added Viewbinding

--- app/src/main/java/com/example/orthoepy/MainActivity.kt
+++ app/src/main/java/com/example/orthoepy/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.orthoepy.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        val navController = findNavController(R.id.fragmentContainerView)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bottomNavigationView = binding.bottomNavigation
+        val navController = findNavController(R.id.fragmentContainerView) // Я обучусь binding с фрагментами
+                                                                          // 9 марта, пока будет так
         bottomNavigationView.setupWithNavController(navController)
     }
 }
