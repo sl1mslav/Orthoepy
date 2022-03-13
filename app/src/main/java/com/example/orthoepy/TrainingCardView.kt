d838eac1450d4af2c2e3762515c34348cf958277
Raffaell <raffaellmir@gmail.com>
2022-03-13 19:13:42 +0500

    Add custom view training_card.xml

---
+++ app/src/main/java/com/example/orthoepy/TrainingCardView.kt
@@ -0,0 +1,45 @@
+package com.example.orthoepy
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.orthoepy.databinding.TrainingCardBinding
+
+class TrainingCardView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding: TrainingCardBinding
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context) : this(context, null)
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        inflater.inflate(R.layout.training_card, this, true)
+        binding = TrainingCardBinding.bind(this)
+        initAttributes(attrs, defStyleAttr)
+    }
+
+    private fun initAttributes(attrs: AttributeSet?, defStyleAttr: Int) {
+        if (attrs == null) return
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.TrainingCardView)
+
+        val labelText = typedArray.getString(R.styleable.TrainingCardView_labelText)
+        binding.labelText.text = labelText ?: "*название_тренировки*"
+
+        val descriptionText = typedArray.getString(R.styleable.TrainingCardView_descriptionText)
+        binding.descriptionText.text = descriptionText ?: "*описание*"
+
+        val icon = typedArray.getDrawable(R.styleable.TrainingCardView_icon)
+        binding.icon.setImageDrawable(icon)
+
+        typedArray.recycle()
+    }
+
+}
