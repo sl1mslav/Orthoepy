69474bc0bc28c7ce10b5cbc6d4eabcbfacb85307
sl1mslav <dm.partilov@gmail.com>
2022-03-19 22:46:49 +0500

    working on store fragment

--- app/src/main/java/com/example/orthoepy/MainActivity.kt
+++ app/src/main/java/com/example/orthoepy/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.orthoepy.adapters.WordsStoreAdapter
 import com.example.orthoepy.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    // MOVE THIS TO STORE FRAGMENT
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val bottomNavigationView = binding.bottomNavigation
-        val navController = findNavController(R.id.fragmentContainerView) // Я обучусь binding с фрагментами
-                                                                          // 9 марта, пока будет так
+        val navController = findNavController(R.id.fragmentContainerView)
         bottomNavigationView.setupWithNavController(navController)
     }
 }
