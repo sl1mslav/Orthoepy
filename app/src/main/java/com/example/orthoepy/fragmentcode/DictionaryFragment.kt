82a71f8a2b0bdbe20192d01b86fd01ba9b1cfe61
sl1mslav <dm.partilov@gmail.com>
2022-04-05 21:10:31 +0500

    updated dictionary fragment

--- app/src/main/java/com/example/orthoepy/fragmentcode/DictionaryFragment.kt
+++ app/src/main/java/com/example/orthoepy/fragmentcode/DictionaryFragment.kt
@@ -6,15 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.orthoepy.R
+import com.example.orthoepy.adapters.DictionaryVPAdapter
+import com.example.orthoepy.databinding.FragmentDictionaryBinding
+import com.example.orthoepy.databinding.FragmentStoreBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class DictionaryFragment : Fragment() {
 
+    private var _binding: FragmentDictionaryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dictionary, container, false)
+        _binding = FragmentDictionaryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val dictionaryVp = binding.dictionaryVp
+        val adapter = DictionaryVPAdapter(this)
+        dictionaryVp.adapter = adapter
+
+        TabLayoutMediator(binding.dictionaryTabs, dictionaryVp) {
+            tab, position ->
+            tab.text = adapter.getTitle(position)
+        }.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
