5e78c67b0855cff6b9f62b97a4a2db40e42a9969
Raffaell <raffaellmir@gmail.com>
2022-04-05 16:36:04 +0500

    Add Room di

--- app/src/main/java/com/example/orthoepy/di/AppComponent.kt
+++ app/src/main/java/com/example/orthoepy/di/AppComponent.kt
@@ -2,6 +2,7 @@ package com.example.orthoepy.di
 
 import com.example.orthoepy.MainActivity
 import com.example.orthoepy.MainViewModel
+import com.example.orthoepy.data.repository.DictionaryRepository
 import com.example.orthoepy.fragmentcode.StoreFragment
 import dagger.Component
 import dagger.Module
@@ -14,5 +15,7 @@ interface AppComponent {
     fun inject(mainActivity: MainActivity)
 
     fun inject(mainViewModel: MainViewModel)
+
+    fun inject(dictionaryRepository: DictionaryRepository)
 
 }
--- app/src/main/java/com/example/orthoepy/di/DataModule.kt
+++ app/src/main/java/com/example/orthoepy/di/DataModule.kt
@@ -1,7 +1,10 @@
 package com.example.orthoepy.di
 
 import android.content.Context
+import com.example.orthoepy.data.database.DictionaryDao
+import com.example.orthoepy.data.database.DictionaryDatabase
 import com.example.orthoepy.data.repository.DatastoreRepository
+import com.example.orthoepy.data.repository.DictionaryRepository
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -13,6 +16,24 @@ class DataModule {
     @Singleton
     fun provideDatastoreRepository(context: Context) : DatastoreRepository {
         return DatastoreRepository(context = context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDictionaryRepository(dictionaryDatabase: DictionaryDatabase) : DictionaryRepository {
+        return DictionaryRepository(dictionaryDao = dictionaryDatabase.dictionaryDao())
+    }
+
+    @Provides
+    @Singleton
+    fun provideDictionaryDatabase(context: Context): DictionaryDatabase {
+        return DictionaryDatabase.getDatabase(context = context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDictionaryDAO(dictionaryDatabase: DictionaryDatabase): DictionaryDao {
+        return dictionaryDatabase.dictionaryDao()
     }
 
 }
