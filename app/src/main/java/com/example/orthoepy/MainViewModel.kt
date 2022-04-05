40e5ff099eed149c39f85c5bb45286aa44cd8931
Raffaell <raffaellmir@gmail.com>
2022-04-05 16:35:32 +0500

    Add Room requests in viewModel

--- app/src/main/java/com/example/orthoepy/MainViewModel.kt
+++ app/src/main/java/com/example/orthoepy/MainViewModel.kt
@@ -1,39 +1,51 @@
 package com.example.orthoepy
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.orthoepy.data.database.Word
 import com.example.orthoepy.data.repository.DatastoreRepository
+import com.example.orthoepy.data.repository.DictionaryRepository
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
-class MainViewModel (application: Application) : AndroidViewModel(application) {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     @Inject lateinit var datastoreRepository: DatastoreRepository
+    @Inject lateinit var dictionaryRepository: DictionaryRepository
+
+    var getAllWords: LiveData<List<Word>>
+    var getBoughtWords: LiveData<List<Word>>
+    var getNotBoughtWords: LiveData<List<Word>>
 
     init {
-        Log.d("MainViewModel", "Букв: ")
         (application as App).getAppComponent().inject(this)
+        getAllWords = getAll()
+        getBoughtWords = getBoughtWords()
+        getNotBoughtWords = getNotBoughtWords()
     }
 
+    //DataStore requests
     fun increaseLetterCount(letters: Int) {
         viewModelScope.launch() {
             datastoreRepository.increaseLetterCount(letters)
         }
     }
-
     fun setLetterCount(letters: Int) {
         viewModelScope.launch() {
             datastoreRepository.setLetterCount(letters)
         }
     }
-
     fun getLetterCount(): Int = runBlocking {
         datastoreRepository.getLetterCount()
     }
 
-    //val getLetterCount = datastoreRepository.getLetterCountFlow()
-
+    //Room requests
+    fun getWordById(id: Int) = dictionaryRepository.getWordById(id).asLiveData()
+    private fun getAll() = dictionaryRepository.allWords.asLiveData()
+    private fun getBoughtWords() = dictionaryRepository.getBoughtWords.asLiveData()
+    private fun getNotBoughtWords() = dictionaryRepository.getNotBoughtWords.asLiveData()
 }
