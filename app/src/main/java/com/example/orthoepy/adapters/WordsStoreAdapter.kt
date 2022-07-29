3b0e97815c397e226c3889c419315421dc32ad72
sl1mslav <dm.partilov@gmail.com>
2022-07-29 18:58:32 +0500

    changed adapter to DiffUtil ListAdapter

--- app/src/main/java/com/example/orthoepy/adapters/WordsStoreAdapter.kt
+++ app/src/main/java/com/example/orthoepy/adapters/WordsStoreAdapter.kt
@@ -1,32 +1,17 @@
 package com.example.orthoepy.adapters
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.orthoepy.R
+import com.example.orthoepy.data.database.Word
 import com.example.orthoepy.databinding.WordItemBinding
-import com.example.orthoepy.wordsmodel.Word
-
-interface WordsStoreActionListener {
-    fun wordStoreClick(word: Word)
-
-    fun wordStoreUnClick(word: Word)
-}
 
 class WordsStoreAdapter(
-    private val actionListener: WordsStoreActionListener
-): RecyclerView.Adapter<WordsStoreAdapter.WordsStoreViewHolder>() {
-
-    var wordsStore: List<Word> = emptyList()
-        set(newValue) {
-            field = newValue
-            notifyDataSetChanged()
-        }
-
+    val onClick: (Word) -> Boolean
+): ListAdapter<Word, WordsStoreViewHolder>(DiffUtilCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsStoreViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -36,37 +21,31 @@ class WordsStoreAdapter(
     }
 
     override fun onBindViewHolder(holder: WordsStoreViewHolder, position: Int) {
-        val wordStore = wordsStore[position]
+        val wordStore = getItem(position)
         with(holder.binding) {
-            holder.itemView.tag = wordStore
             wordInList.text = wordStore.wordText
-            checkbox.isChecked = wordStore.isChecked
-        }
-
-    }
-
-    override fun getItemCount(): Int = wordsStore.size
-
-    inner class WordsStoreViewHolder(
-        val binding: WordItemBinding
-    ): RecyclerView.ViewHolder(binding.root), View.OnClickListener {
-
-        init {
-            binding.root.setOnClickListener(this)
-        }
-
-        override fun onClick(p0: View) {
-            val wordBinding = WordItemBinding.bind(p0)
-            val position = adapterPosition
-            val wordStore = wordsStore[position]
-            if (position != RecyclerView.NO_POSITION) {
-                if (!wordBinding.checkbox.isChecked)
-                    actionListener.wordStoreClick(wordStore)
-                else
-                    actionListener.wordStoreUnClick(wordStore)
-                wordStore.isChecked = !wordStore.isChecked
-                wordBinding.checkbox.isChecked = wordStore.isChecked
+            checkbox.isChecked = wordStore.isChecked.toBoolean()
+            root.setOnClickListener {
+                wordStore?.let {
+                    if (onClick(it))
+                        checkbox.isChecked = !checkbox.isChecked
+                }
+            }
+            checkbox.setOnClickListener {
+                wordStore?.let {
+                    if (!onClick(it)) {
+                        checkbox.isChecked = false
+                        checkbox.jumpDrawablesToCurrentState()
+                    }
+                }
             }
         }
     }
 }
+class DiffUtilCallback : DiffUtil.ItemCallback<Word>() {
+    override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean = oldItem == newItem
+    override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean =
+        oldItem.id == newItem.id
+}
+
+class WordsStoreViewHolder(val binding: WordItemBinding) : RecyclerView.ViewHolder(binding.root)
