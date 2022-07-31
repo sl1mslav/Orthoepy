530fae25059003ae85b8ce9ec5e22d8485523842
sl1mslav <dm.partilov@gmail.com>
2022-08-01 02:32:39 +0500

    new colors for memorization levels

---
+++ app/src/main/java/com/example/orthoepy/entity/WordCardColors.kt
@@ -0,0 +1,10 @@
+package com.example.orthoepy.entity
+import com.example.orthoepy.R
+
+enum class WordCardColors(val color: Int) {
+    RED(R.color.red),
+    PINK(R.color.pink),
+    YELLOW(R.color.yellow),
+    LIGHT_GREEN(R.color.light_green),
+    GREEN(R.color.green)
+}
