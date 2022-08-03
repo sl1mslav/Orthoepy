7cb70313037b0b5cf335c7bdfa85b841a50ed487
sl1mslav <dm.partilov@gmail.com>
2022-08-03 19:22:18 +0500

    irrelevant changes (moving and rearranging some files)

--- app/src/main/java/com/example/orthoepy/adapters/DictionaryVPAdapter.kt
+++ app/src/main/java/com/example/orthoepy/adapters/DictionaryVPAdapter.kt
@@ -2,9 +2,9 @@ package com.example.orthoepy.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.orthoepy.fragmentcode.DictionaryClassic
-import com.example.orthoepy.fragmentcode.DictionaryExam
-import com.example.orthoepy.fragmentcode.DictionaryPersonal
+import com.example.orthoepy.fragmentcode.dictionary.DictionaryClassic
+import com.example.orthoepy.fragmentcode.dictionary.DictionaryExam
+import com.example.orthoepy.fragmentcode.dictionary.DictionaryPersonal
 
 class DictionaryVPAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
