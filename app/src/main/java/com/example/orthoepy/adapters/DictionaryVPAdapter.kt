b1727d4496298044366bf0e7bc93e668957c4218
sl1mslav <dm.partilov@gmail.com>
2022-08-07 12:30:20 +0500

    added ability to get favourite/exam words from DB

--- app/src/main/java/com/example/orthoepy/adapters/DictionaryVPAdapter.kt
+++ app/src/main/java/com/example/orthoepy/adapters/DictionaryVPAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.orthoepy.adapters
 
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.orthoepy.fragmentcode.dictionary.DictionaryClassic
