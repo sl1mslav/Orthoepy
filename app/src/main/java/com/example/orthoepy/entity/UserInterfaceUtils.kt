4a074e9bc6e3f9fd4bf789884b23a653ff886e9a
sl1mslav <dm.partilov@gmail.com>
2022-08-15 13:52:05 +0500

    utils now support adding a chip to a flow virtual widget object

--- app/src/main/java/com/example/orthoepy/entity/UserInterfaceUtils.kt
+++ app/src/main/java/com/example/orthoepy/entity/UserInterfaceUtils.kt
@@ -2,15 +2,21 @@ package com.example.orthoepy.entity
 
 import android.app.Activity
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.constraintlayout.helper.widget.Flow
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.viewpager2.widget.ViewPager2
+import com.example.orthoepy.R
+import com.google.android.material.chip.Chip
 import kotlinx.coroutines.launch
 
 object UserInterfaceUtils {
@@ -44,5 +50,24 @@ object UserInterfaceUtils {
 
     fun ViewPager2.getCurrentFragment(fragmentManager: FragmentManager): Fragment? {
         return fragmentManager.findFragmentByTag("f$currentItem")
+    }
+
+    fun Flow.addChip(context: Context, text: String, isCorrectLetter: Boolean) {
+        val parentView = parent as ViewGroup
+        val view = LayoutInflater.from(context).inflate(R.layout.chip_letter_item, parentView, false)
+        view.id = View.generateViewId()
+        (view as Chip).text = text
+        parentView.addView(view)
+        addView(view)
+        if (isCorrectLetter) {
+            view.setOnClickListener {
+                Toast.makeText(context, "Correct!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else {
+            view.setOnClickListener {
+                Toast.makeText(context, "Incorrect!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
